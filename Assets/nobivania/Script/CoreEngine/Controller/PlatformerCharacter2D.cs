8368dee40f948b0b170e6a878c1f4ccd175bd92d
karorogunso <karorogunsou@gmail.com>
2018-02-25 18:24:27 +0700

    [Update/Issues] Nobita Sprite, Animation, Controller
    Need more work on Nobita and Controller

--- Assets/nobivania/Script/CoreEngine/Controller/PlatformerCharacter2D.cs
+++ Assets/nobivania/Script/CoreEngine/Controller/PlatformerCharacter2D.cs
@@ -48,7 +48,7 @@ namespace UnityStandardAssets._2D
         {
             // Setting up references.
             m_GroundCheck = transform.Find("GroundCheck");
-            m_CeilingCheck = transform.Find("CeilingCheck");
+            //m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
         }
