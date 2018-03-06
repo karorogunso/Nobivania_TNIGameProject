cda64762c33cdc237e0900c896a229e5902d7292
P <na.pattara_st@tni.ac.th>
2018-03-06 14:40:41 +0700

    Shouting text giant

---
+++ Assets/nobivania/Script/CoreEngine/Controller/SoundObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundObject : MonoBehaviour {
+
+    public float speed;
+    Rigidbody2D rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+	
+	// Update is called once per frame
+    void Update () {
+	
+    }
+
+    // Kill bullet once offscreen
+    void OnBecameInvisible () {
+        //Debug.Log("bullet offscreen");
+        Destroybullet();
+    }
+    void OnCollisionEnter2D()
+    {
+        //Debug.Log("Collider");
+        Destroybullet();
+    }
+    void Destroybullet()
+    {
+        //rb.simulated = false;
+        Destroy(gameObject);
+    }
+}
