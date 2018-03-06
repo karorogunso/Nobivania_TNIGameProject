b84b94b05688300978d74475933e9966429e9c1e
P <na.pattara_st@tni.ac.th>
2018-03-06 16:43:37 +0700

    giant scene test

---
+++ Assets/nobivania/Script/CoreEngine/Controller/Giant_Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Giant_Spawner : MonoBehaviour {
+
+	public GameObject prefab;
+	public float delay;
+	public float period;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("LaunchProjectile", delay, period);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void LaunchProjectile()
+	{
+		Instantiate(prefab);
+	}
+}
--- Assets/nobivania/Script/CoreEngine/Controller/SoundObject.cs
+++ Assets/nobivania/Script/CoreEngine/Controller/SoundObject.cs
@@ -7,11 +7,12 @@ public class SoundObject : MonoBehaviour {
     public float speed;
     public float miny;
     public float maxy;
+    public float startx;
     Rigidbody2D rb;
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
-        transform.position = new Vector3(transform.position.x, Random.Range(miny,maxy), 0);
+        transform.position = new Vector3(startx, Random.Range(miny,maxy), 0);
         rb.velocity = transform.right * speed;
     }
 	
@@ -25,14 +26,24 @@ public class SoundObject : MonoBehaviour {
         //Debug.Log("bullet offscreen");
         Destroybullet();
     }
-    void OnCollisionEnter2D()
+    void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log("Collider");
-        Destroybullet();
+        //Destroybullet();
+        //Debug.Log(GetCollisionAngle(collision));
     }
     void Destroybullet()
     {
         //rb.simulated = false;
         Destroy(gameObject);
     }
+    /*public float GetCollisionAngle(Transform hitobjectTransform, CircleCollider2D collider, Vector2 contactPoint)
+    {
+        Vector2 collidertWorldPosition = new Vector2(hitobjectTransform.position.x, hitobjectTransform.position.y);
+        Vector3 pointB = contactPoint - collidertWorldPosition;
+        float theta = Mathf.Atan2(pointB.x, pointB.y);
+        float angle = (360 - ((theta * 180) / Mathf.PI)) % 360;
+        return angle;
+    }*/
+
 }
