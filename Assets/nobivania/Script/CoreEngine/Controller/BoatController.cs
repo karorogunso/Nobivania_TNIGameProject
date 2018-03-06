d524dd14b496148939b8d0fcb85cad814db329bd
Free <lifeistfree@gmail.com>
2018-03-06 12:58:46 +0700

    boat

--- Assets/nobivania/Script/CoreEngine/Controller/BoatController.cs
+++ Assets/nobivania/Script/CoreEngine/Controller/BoatController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class BoatController : MonoBehaviour {
 
+    public Vector3 floor;
 	// Use this for initialization
 	void Start () {
-		
+        floor = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnPlayerAttach()
+    {
+
+    }
 }
