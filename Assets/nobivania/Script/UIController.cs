77ff210f2a7974bad62954745b9a4b816ef6e43f
Free <lifeistfree@gmail.com>
2018-03-07 14:29:52 +0700

    main ui

---
+++ Assets/nobivania/Script/UIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+	public void OnStartClick()
+    {
+        SceneManager.LoadScene("Act1_Scene1");
+    }
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+}
