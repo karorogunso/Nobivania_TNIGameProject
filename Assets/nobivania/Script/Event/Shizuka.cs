d2f11ae0bc614dcef73821309d873fff9e059b51
Free <lifeistfree@gmail.com>
2018-03-07 11:10:21 +0700

    shizuka,level 1

--- Assets/nobivania/Script/Event/Shizuka.cs
+++ Assets/nobivania/Script/Event/Shizuka.cs
@@ -9,7 +9,7 @@ public class Shizuka : MonoBehaviour {
 
     public GameObject RequestPopup;
     public DoorController Door;
-
+    public AudioClip ThankyouSound;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
@@ -29,5 +29,11 @@ public class Shizuka : MonoBehaviour {
         UI = GameObject.Find("ItemUI").GetComponent<Image>();
         UI.color = new Color(1, 1, 1, 0);
         Door.IsLock = false;
+        if (ThankyouSound)
+        {
+            AudioSource source = GetComponent<AudioSource>();
+            source.PlayOneShot(ThankyouSound);
+        }
+        
     }
 }
